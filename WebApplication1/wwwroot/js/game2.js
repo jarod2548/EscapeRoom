11dd56f7fecd27afab43d9f9b1a3b6d5af0c7a03
jarod2548 <jarodkonijnenberg@gmail.com>
2025-05-08 10:48:21 +0200

    update

--- WebApplication1/wwwroot/js/game2.js
+++ WebApplication1/wwwroot/js/game2.js
@@ -9,3 +9,13 @@ function player1Start()
         gameScreen.style.display = 'block';
     }
 }
+function randomizeColors() {
+    // Define an array of colors
+    const colors = ['#ff6347', '#4682b4', '#32cd32', '#ffb6c1', '#ff1493', '#8a2be2'];
+
+    // Randomly assign colors to each quadrant
+    document.querySelector('.top-left').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector('.top-right').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector('.bottom-left').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector('.bottom-right').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+}
