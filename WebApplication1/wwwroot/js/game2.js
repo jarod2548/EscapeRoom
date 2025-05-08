d737f8135d7ee2b11e803d7989971548d39ddf60
jarod2548 <jarodkonijnenberg@gmail.com>
2025-05-08 10:16:12 +0200

    update

--- WebApplication1/wwwroot/js/game2.js
+++ WebApplication1/wwwroot/js/game2.js
@@ -1,3 +1,11 @@
 ﻿const gameScreen = document.getElementById('gameScreen');
 const player1BTN = document.getElementById('player1Button');
 const player2BTN = document.getElementById('player2Button');
+
+function player1Start()
+{
+    if (gameScreen.style.display === 'none')
+    {
+        gameScreen.style.display = 'block';
+    }
+}
