f585380fdaba409a094d447e0278338063543202
jarod2548 <jarodkonijnenberg@gmail.com>
2025-05-08 09:07:15 +0200

    n

---
+++ WebApplication1/wwwroot/js/game2.js
@@ -0,0 +1,3 @@
+﻿const gameScreen = document.getElementById('gameScreen');
+const player1BTN = document.getElementById('player1Button');
+const player2BTN = document.getElementById('player2Button');
