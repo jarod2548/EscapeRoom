b5e16ea0a3bf7ff2553d088da5f7f59ea229266c
jarod2548 <jarodkonijnenberg@gmail.com>
2025-05-12 13:24:28 +0200

    My changes

--- WebApplication1/GameState.cs
+++ WebApplication1/GameState.cs
@@ -1,9 +1,12 @@
-﻿namespace WebApplication1
+﻿using WebApplication1.services;
+
+namespace WebApplication1
 {
     public class GameState
     {
-        public GameState() 
+        public GameState(string newID) 
         {
+            ID = newID;
             CreateLightsGame();
         }
 
@@ -11,16 +14,34 @@
         public int button1 { get; set; }
         public int button2 { get; set; }
 
+        public int randomInt { get; set; }
+
+        public List<int[]> buttons = new List<int[]>
+        {
+            new int[]{0,1},
+            new int[]{2,3},
+            new int[]{1,2},
+            new int[]{0,3},
+            new int[]{3,1},
+        };
+
+        public List<int> buttonToUse = new List<int>();
+
+        Random rand = new Random();
+
         public void CreateLightsGame() 
-        { 
-            ID = Guid.NewGuid().ToString();
-            button1 = 0;
-            button2 = 1;
+        {                     
+            randomInt = rand.Next(buttons.Count);
+            button1 = buttons[randomInt][0];
+            button2 = buttons[randomInt][1];
+            buttonToUse.Add(button1);
+            buttonToUse.Add(button2);
+            
         }
-        public async Task CheckLights(int BTNpressed) 
+        public async Task CheckLights(int BTNpressed, int gameOrder) 
         {
             Console.WriteLine("Button pressed on site");
-            if (BTNpressed == button1) 
+            if (buttonToUse[gameOrder] == BTNpressed) 
             { 
                 Console.WriteLine("Correct button");
             }
